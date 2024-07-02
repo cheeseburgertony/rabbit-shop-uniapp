207b108cfe96f1c5fa0e38b23c7063202f8c24b9
cheeseburgertony <1821955020@qq.com>
2024-07-02 15:19:26 +0800

    feat:轮播图指示点

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -2,11 +2,17 @@
 import { ref } from 'vue'
 
 const activeIndex = ref(0)
+
+// 引用unihelper自带的类型声明文件
+const onChange: UniHelper.SwiperOnChange = (e) => {
+  // !代表非空断言，表示当前这个不可能为空了，主观上排除空值情况
+  activeIndex.value = e.detail!.current
+}
 </script>
 
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="false" :interval="3000">
+    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
       <swiper-item>
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
           <image
