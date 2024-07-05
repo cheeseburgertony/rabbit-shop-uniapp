21cdd5c7c27e11ece30b93614a49970401856460
cheeseburgertony <1821955020@qq.com>
2024-07-05 13:49:29 +0800

    feat:SKU组件-渲染被选中的值

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -2,11 +2,14 @@
 import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import AddressPanel from './components/AddressPanel.vue'
 import ServicePanel from './components/ServicePanel.vue'
 import PageSkeleton from './components/PageSkeleton.vue'
-import type { SkuPopupLocaldata } from '@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup'
+import type {
+  SkuPopupInstance,
+  SkuPopupLocaldata,
+} from '@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -87,6 +90,13 @@ const openSkuPopup = (val: SkuMode) => {
   isShowSku.value = true
   mode.value = val
 }
+
+// Sku组件实例
+const skuPopupRef = ref<SkuPopupInstance>()
+// 通过计算属性计算被选中的值
+const selectArrText = computed(
+  () => skuPopupRef.value?.selectArr?.join(' ').trim() || '请选择商品规格',
+)
 </script>
 
 <template>
@@ -97,6 +107,12 @@ const openSkuPopup = (val: SkuMode) => {
     :mode="mode"
     add-cart-background-color="#ffa868"
     buy-now-background-color="#27ba9b"
+    ref="skuPopupRef"
+    :actived-style="{
+      color: '#27BA9B',
+      borderColor: '#27BA9B',
+      backgroundColor: '#E9F8F5',
+    }"
   />
 
   <scroll-view v-if="isFinish" scroll-y class="viewport">
@@ -130,7 +146,7 @@ const openSkuPopup = (val: SkuMode) => {
       <view class="action">
         <view class="item arrow">
           <text class="label">选择</text>
-          <text class="text ellipsis" @tap="openSkuPopup(SkuMode.Both)"> 请选择商品规格 </text>
+          <text class="text ellipsis" @tap="openSkuPopup(SkuMode.Both)"> {{ selectArrText }} </text>
         </view>
         <view class="item arrow" @tap="onPopup('address')">
           <text class="label">送至</text>
